aa23b6e0699024bc28016fccf14af7fb3301b6d3
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-09-21 16:02:20 +0300

    -

---
+++ Frontendbasics/buutti/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import img from './Sieppaa.png'
+
+function App() {
+  return (
+    <div className="App">
+            <p>This is a paragraph.</p>
+      <p>This is a paragraph.</p>
+      <p>This is a paragraph.</p>
+      <a href="www.google.com">link text</a>
+      <img height="100" width="100" src={img}></img>
+      <input placeholder ="firstName"></input>
+      <input placeholder ="lastName"></input>
+      <input type="radio" value="HTML"></input>
+      <button> Submit</button>
+      <button> Reset</button>
+
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+    </div>
+  );
+}
+
+export default App;
