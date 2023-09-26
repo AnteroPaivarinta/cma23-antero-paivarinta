655ec19e728796e9cb19df27c281cedc28f83843
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-09-26 11:03:14 +0300

    koodia

--- ReactBasics/3/src/App.js
+++ ReactBasics/3/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-
+import TodoNote from './TodoNote';
+import { v4 as uuidv4 } from 'uuid';
 function App() {
   const defaultTodos = [
     {id: 1, text: 'Buy potatoes', complete: false},
@@ -12,12 +13,34 @@ function App() {
     {id: 6, text: 'Play videogames', complete: true},
   ]
   const [arrayData, setArrayData ] = useState(defaultTodos.map((value) =>  ({...value, remove: false })));
+  const [text, setText ] = useState('');
 
-  const remove = (value) => {
+  const toggleCompletion = (id) => {
+    const index = arrayData.findIndex((value) => value.id === id);
     const copy = [...arrayData];
-    let bool = copy[value].remove
-    copy[value] = {...copy[value], remove: !bool};
+
+    let bool = copy[index].complete
+    copy[index] = {...copy[index], complete: !bool};
     setArrayData(copy);
+  }
+
+  const addTodo = (text) => {
+    setArrayData([...arrayData, {
+      id: uuidv4(),
+      complete: false,
+      text: text,
+    }])
+  }
+
+
+  const remove = (value, index) => {
+    const id = value.id
+    const copy = [...arrayData];
+    let bool = copy[index].remove
+    copy[index] = {...copy[index], remove: !bool};
+    const filtered = copy.filter((value) => value.id !== id);
+    console.log('Filtered', filtered)
+    setArrayData(filtered);
   }
   
   useEffect(() => {
@@ -25,21 +48,27 @@ function App() {
   }, [arrayData]);
  
 
-  const array = arrayData.map((value, index) => <div key={value.id} style={{backgroundColor: 'purple', width: "30%", height: "100px", margin: 10, display: 'flex', flexDirection: 'row'}}>
-    <input type='checkbox'></input>
-    <div key = {value.id}>{value.text}  {value.remove.toString()}</div>
-    <label>
-      <input onClick={() => remove(index)} type="checkbox" checked={false}  />
-      { value.remove === true && <div style={{margin: -15, color:'red', fontSize: 10}}> X</div>} 
-    </label>
-  </div>)
+  const array = arrayData.map((value, index) => 
+    <TodoNote index={index} value={value} remove={remove} toggleCompletion={toggleCompletion} />
+  )
 
   return (
     <div className="App">
-      <h1>Assignment 1</h1>
+      <h1>Assignment 1, 2, 4, 5</h1>
       <hr></hr>
-      <div style={{flexDirection: 'column', height: "30%", width: "60%"}}>
-        {array}
+      <div style={{display:'flex' , flexDirection: 'row', height: "69%", width: "100%"}}>
+        <div style={{ display:'flex' , flexDirection: 'column', height:  '100vh', width: "30%"}}>
+          {array} 
+        </div>
+        <div style={{ display:'flex' , flexDirection: 'column', height: '100vh', width: '30%', alignContent: 'center'}}>
+          <label>
+            Title: 
+            <input value={text} onChange={(e) => setText(e.target.value)}></input>
+          </label>
+          <button onClick={() => addTodo(text)}>Submit</button>
+         
+        </div>
+        
       </div>
     </div>
   );
--- ReactBasics/3/src/TodoNote.js
+++ ReactBasics/3/src/TodoNote.js
@@ -1,7 +1,15 @@
-const defaultTodos = (props) => {
+const TodoNote = (props) => {
     return(
-        <div>
-
-        </div>
+    <div key={props.value.id} style={{backgroundColor: 'purple', width: "30%", height: "100px", margin: 10, display: 'flex', flexDirection: 'row'}}>
+        <input onClick={() => props.toggleCompletion(props.value.id)} checked={props.value.complete} type='checkbox'></input>
+        <div key = {props.value.id}>{props.value.text} </div>
+        <label>
+          <input onClick={() => props.remove(props.value, props.index)} type="checkbox" checked={false}  />
+          {props.value.remove === true && <div style={{margin: -15, color:'red', fontSize: 10}}> X</div>} 
+        </label>
+      </div>
     )
 }
+
+
+export default TodoNote;
