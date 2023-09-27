364a3f2033fc99432a2a13ff0cac26585c9076a3
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-09-27 14:15:15 +0300

    juu

---
+++ csharp/7-classes-and-objects/ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+class User
+{
+    public string user;
+    public string password;// Accessible only inside the class
+                           // Accessible everywhere within the namespace
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        User a = new User();
+        Console.WriteLine("username");
+        string name = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("Password");
+        string pw = Convert.ToString(Console.ReadLine());
+
+
+
+        a.user = name;
+        a.password = pw;
+
+        Console.WriteLine("Nämä ovat tietosi: " + a.user + " ja " + a.password);
+
+
+
+
+
+    }
+}
---
+++ csharp/7-classes-and-objects/ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+class Animal
+{
+    public string name;
+    public string sound;
+    public Animal(string name, string sound)
+    {
+        this.name = name;
+        this.sound = sound;
+    }
+    public void Greet()
+    {
+        Console.WriteLine(this.name + "says " + this.sound);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Animal A = new Animal("joo", "BADABIMBADABUm");
+        Animal B = new Animal("OK", "BADABIMBADABUm");
+        A.Greet();
+        B.Greet();
+
+    
+
+
+
+
+
+
+
+    }
+}
