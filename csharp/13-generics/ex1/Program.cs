cb342dee1efe94f40dbde81fe898163db646d594
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-09-28 19:16:01 +0300

    T2

---
+++ csharp/13-generics/ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+public class Program
+{
+    public static void Main()
+    {
+        List<string> stringList = GetPopulatedList<string>("Hello, there", 5);
+        foreach (string value in stringList)
+        {
+            Console.WriteLine(value);
+        }
+
+        List<int> intList = GetPopulatedList<int>(42, 3);
+        foreach (int value in intList)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    public static List<T> GetPopulatedList<T>(T value, int length)
+    {
+        List<T> resultList = new List<T>(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            resultList.Add(value);
+        }
+
+        return resultList;
+    }
+}
