5a959bca9a23ff78e043ede41b4d6ff101825e54
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-09-28 14:38:39 +0300

    tehtäviä

---
+++ csharp/12-files/ex1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+List<string> list = new List<string>();
+while (true)
+{
+    Console.WriteLine("Anna merkkijono: ");
+    string x = Console.ReadLine();
+    if(x == " ")
+    {
+        Console.WriteLine("Anna tiedoston nimi: ");
+        string file = Console.ReadLine();
+        string path = @"C:\code\"+file+".txt";
+
+        if (File.Exists(path))
+        {
+            Console.WriteLine($"The file {path} does not exist.");
+        }
+        else
+        {
+
+            try
+            {
+                // Avataan tiedosto kirjoittamista varten, luomme sen, jos se ei ole olemassa
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    // Kirjoitetaan teksti tiedostoon
+                    for( int i =0; i < list.Count; i++)
+                    {
+                        sw.WriteLine(list[i]);
+
+                    }
+                }
+                Console.WriteLine("Teksti kirjoitettu onnistuneesti tiedostoon.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Virhe: {ex.Message}");
+            }
+        }
+
+        break;
+
+    }
+    else
+    {
+        list.Add(x);
+    }
+}
