8ee6fec3abd4e372eea6e1982f482782294db18a
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-09-29 23:22:14 +0300

    Tehtäviä!

---
+++ csharp/14/ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                Thread loaderThread = new Thread(LoadData);
+                loaderThread.Start();
+            }
+        }
+    }
+
+  
+
+    public static void LoadData()
+    {
+        int progress = 0;
+        while (progress <= 100)
+        {
+            Console.WriteLine("Loading: " + progress + "%");
+            progress++;
+            Thread.Sleep(10);
+        }
+        Console.WriteLine("Resource loading complete!");
+    }
+}
---
+++ csharp/14/ex3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Press Enter to start resource loading...");
+
+        while (true)
+        {
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                Thread loaderThread = new Thread(LoadDataAsync);
+                loaderThread.Start();
+            }
+        }
+    }
+
+    public async static void LoadDataAsync()
+    {
+        int progress = 0;
+        while (progress <= 100)
+        {
+            Console.WriteLine("Loading: " + progress + "%");
+            progress++;
+            Thread.Sleep(10);
+        }
+        Console.WriteLine("Resource loading complete!");
+    }
+}
