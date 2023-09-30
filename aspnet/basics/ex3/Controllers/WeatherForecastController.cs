1ecc65540d0bd46231b9f9d3902e5eca2b9053b9
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-09-30 17:38:10 +0300

    ex3

---
+++ aspnet/basics/ex3/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ex3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public List<string> Get()
+        {
+            List<string> a = new List<string>();
+            a.Add("jep");
+            return a;
+        }
+    }
+}
