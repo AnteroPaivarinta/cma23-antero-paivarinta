2c02f89c0c7caa9b0853959fa587452ade87c753
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-10-04 01:21:05 +0300

    7 kappaleen ex5

---
+++ aspnet/7-database/ex5/ex1/Repositories/CourseRepository.cs
@@ -0,0 +1,29 @@
+﻿using ex1.Models;
+using ex1.Services;
+
+namespace ex1.Repositories
+{
+    public class CourseRepository: ICourseRepository
+    {
+        private readonly CoursesContext _context;
+        public List<Course> courses { get; set; }
+        private CourseRepository(CoursesContext context)
+        {
+            _context = context;
+           
+            
+        }
+        public List<Course> GetCourses() => courses;
+        public Course GetCourse(int id) =>_context.courses.FirstOrDefault(c => c.id == id);
+        public List<Course> GetContacts() => _context.courses.ToList();
+        public void AddContact(CoursesContext course)
+        {
+            _context.courses.Add(course);
+            _context.SaveChanges();
+        }
+
+
+
+
+    }
+}
