cd9dda1e1d7e55776030e0ada683a86ab0b2ec02
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-10-03 22:11:32 +0300

    ex4 program.cs

--- aspnet/7-database/ex1/Program.cs
+++ aspnet/7-database/ex1/Program.cs
@@ -2,9 +2,7 @@ using ex1.Services;
 using ex1.Repositories;
 using ex1.Models;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ICourseRepository, CourseRepository>();
 builder.Services.AddDbContext<CoursesContext>(options => options.UseNpgsql(@"Server=PostgreSQL 16;Host=localhost;Port=5432;Username=postgres;Password='';Database=testi"));
@@ -13,11 +11,8 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers().AddNewtonsoftJson();
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,9 +20,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
