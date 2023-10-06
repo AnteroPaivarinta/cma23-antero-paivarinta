80103d0029ec3420ddeb6417d05685820bfea1c9
AnteroPaivarinta <antsapaivarinta@gmail.com>
2023-10-06 08:22:58 +0300

    Part 5. edellisiä

--- aspnet/7-database/ex6/ex1/Models/Course.cs
+++ aspnet/7-database/ex6/ex1/Models/Course.cs
@@ -1,21 +1,25 @@
-﻿namespace ex1.Models
+﻿using Microsoft.EntityFrameworkCore;
+using static Npgsql.PostgresTypes.PostgresCompositeType;
+
+namespace ex1.Models
 {
     public class Course
     {
-        public int id;
+        public int id { get; set; }
         public string name;
         public int credits;
-       // public ICollection<Lecture> Lectures;
+        public ICollection<Lecture> Lectures;
+        protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .HasKey(b => b.id);
+        }
 
         public Course(int id, string name, int credits)
         {
             this.id = id;
             this.name = name;
             this.credits = credits;
-        }
-        public Course()
-        {
-           
         }
     }
 }
